43e0b3753857a9c8398b96ab1c6b6f24dd219424
Luck19921 <ohsoso27@gmail.com>
2020-11-26 13:41:38 +0800

    This is commit test for atom editor.

--- 3-打印/Snprintf.c
+++ 3-打印/Snprintf.c
@@ -4,22 +4,23 @@
 
 int main()
 {
+    //如何初始化? 三種方式->1.宣告就初始 2.memset 3.bzero
     char buf[256];
     char buf2[256];
-    char buf3[256] = {0};
+    char buf3[256] = {0}; //第一種初始化方法
     char buf4[256];
     char temp[128] = {0};
-
-    bzero(buf4, sizeof(buf4));
-
-    sprintf(temp, "pf_rule%d", 5);
-    snprintf(buf4, sizeof(buf4), "%s", cms_get_value(temp));
-
+    //bzero(buf4, sizeof(buf4)); //第二種初始化方法, 但bzero是Unix用法, 一般Windows都會使用memset
+    memset(buf4, '\0', sizeof(buf4)); //第三種初始化方法
+    sprintf(temp, "pf_rule_%d", 5);
     printf("temp: %s\n", temp);
-    printf("buf4: %s\n", buf4);
+    //snprintf(buf4, sizeof(buf4), "%s", cms_get_value(temp));
+    snprintf(buf4, sizeof(buf4), "%s", "Hey Hey Hey");
+
+    printf("This is a test\n");
 
     memset(buf, '\0', sizeof(buf));
-    bzero(buf2, sizeof(buf2));
+    memset(buf2, '\0', sizeof(buf2));
 
     sprintf(buf, "%d", 1024);
     sprintf(buf2, "%s", "This is buf2...");
@@ -29,17 +30,25 @@ int main()
     printf("Buf: %s\n", buf);
     printf("Buf2: %s\n", buf2);
     printf("Buf3: %s\n", buf3);
+    printf("Buf4: %s\n", buf4);
 
     printf("\n");
     printf("即將進行重置各buf array的記憶體空間...\n");
+    /*
     bzero(buf, sizeof(buf));
     bzero(buf2, sizeof(buf2));
     bzero(buf3, sizeof(buf3));
+    */
+    memset(buf, '\0', sizeof(buf));
+    memset(buf2, '\0', sizeof(buf2));
+    memset(buf3, '\0', sizeof(buf3));
+    memset(buf4, '\0', sizeof(buf4));
     printf("\n");
 
     printf("===第二次Printf===\n");
     printf("Buf: %s\n", buf);
     printf("Buf2: %s\n", buf2);
     printf("Buf3: %s\n", buf3);
+    printf("Buf4: %s\n", buf4);
     printf("這是三種初始化，避免殘值留存於記憶體中又誤引用的方法。\n");
 }
