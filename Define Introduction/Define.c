5120e3870ec62b38064e67c34cd42b5190189483
Luck19921 <ohsoso27@gmail.com>
2020-12-01 12:32:44 +0800

    Daily Updating #4. Define宏定量宣告

---
+++ Define Introduction/Define.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define year 2020
+#define month 12
+#define day 1
+#define name "Albert Cheng"
+#define url "http:www.google.com"
+
+
+
+int main()
+{
+  printf("The document was established on %d/%d/%d...\n", year, month, day);
+  printf("Author: %s\nReferences: %s\n", name, url);
+
+
+}
