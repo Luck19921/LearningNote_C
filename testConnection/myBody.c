6c9e11a05d71bdaffb6ffbeb4d17eb7ce0934a8e
Luck19921 <ohsoso27@gmail.com>
2020-12-04 11:45:26 +0800

    Makefile by Tic Da Da.

--- testConnection/myBody.c
+++ testConnection/myBody.c
@@ -8,8 +8,8 @@ int main(void)
   int i;
   for (i=0; i<= 10; i++)
   {
-    //printf("%2d%5d\n", i, func1(2, i));
-    printf("%s\n", MYNAME);
+    printf("%2d%5d\n", i, func1(2, i));
+    ///printf("%s\n", MYNAME);
   }
 
   return 0;
