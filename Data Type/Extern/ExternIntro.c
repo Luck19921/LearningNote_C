8d4909310ad43bb538460f703fcd51d7cb7cf635
Luck19921 <ohsoso27@gmail.com>
2020-12-14 14:39:51 +0800

    Extern Code說明修正。

--- Data Type/Extern/ExternIntro.c
+++ Data Type/Extern/ExternIntro.c
@@ -17,6 +17,6 @@ int main()
 /*
 想要呼叫其他檔案內的變數，首先你必須Include該檔案，像是本例子就是include "檔案名稱"
 如果在相同資料夾可以直接include檔案名稱即可，但若是有分上下層資料夾，必須在檔案名稱前加上檔案的路徑
-關於當前資料夾呼叫其他C file的變數，請看StaticV2.c
-關於呼叫下一層資料夾內的C file的變數，請看StaticRootTest/StaticTest.c
+關於當前資料夾呼叫其他C ExternIntro_test1.c
+關於呼叫下一層資料夾內的C file的變數，請看StaticRootTest/ExternIntro_test2.c
 */
