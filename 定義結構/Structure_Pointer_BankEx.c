e88fc50cbde7e93cd3a881dfeecd12f9d2b92783
Luck19921 <ohsoso27@gmail.com>
2020-12-15 19:11:13 +0800

    Based on a Bank Account Example to introduce what is the structure and pointer variable, function.

--- 定義結構/Structure_Pointer_BankEx.c
+++ 定義結構/Structure_Pointer_BankEx.c
@@ -36,22 +36,48 @@ void withdraw(Account *acct, double amount) {
     acct->balance -= amount;
 }
 
+String to_str(Account *acct) {
+    int n = snprintf(NULL, 0,
+        "Account(%s, %s, %f)",
+        acct->id, acct->name, acct->balance
+    );
+
+    if(acct->_to_str != NULL) {
+        free(acct->to_str);
+    }
+
+    acct->_to_str = malloc((n + 1) * sizeof(char));
+    snprintf(acct->_to_str, n + 1,
+        "Account(%s, %s, %f)",
+        acct->id, acct->name, acct->balance
+    );
+
+    return acct->_to_str;
+}
+
 Account* new_account (String id, String name, double balance) {
   Account *acct = (Account*) malloc(sizeof(Account));
   acct->id = id;
   acct->name = name;
   acct->balance = balance;
+  acct->to_str = NULL;
 
   acct->deposit = deposit;
   acct->withdraw = withdraw;
-
+  acct->to_str = to_str;
   return acct;
+}
+
+void del_account(Account *acct) {
+  free(acct->to_str);
+  free(acct);
 }
 
 int main() {
   Account *acct = new_account("123-123-123", "Yamasaki", 1000);
   acct->deposit(acct, 700);
   acct->withdraw(acct, 200);
-  //printf("%s\n")
+  printf("%s\n", acct->to_str(acct));
+  del_account(acct);
   return 0;
 }
