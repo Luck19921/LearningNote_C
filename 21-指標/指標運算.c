f6c16f176b2535756f465f791656c91404a7224d
Luck19921 <ohsoso27@gmail.com>
2020-12-21 19:10:12 +0800

    指標運算與型別運算的說明與更新。

---
+++ 21-指標/指標運算.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(void) {
+  int *p = 0; //沒有地址
+  char *c = 0;
+  char myBook = 'A';
+  printf("p addr: %p\n", p);
+  printf("P value: %d\n", p);
+  printf("P+1 addr: %p\n", p+1);
+  printf("P+1 value: %d\n", p+1);
+  printf("P+2 addr: %p\n", p+2);
+  printf("P+2 value: %d\n", p+2);
+
+  printf("myBook addr: %p\n", myBook);
+  printf("myBook value: %c\n", myBook);
+
+  c = &myBook; //c 取 myBook位址
+
+  printf("c addr: %p\n", c);
+  printf("c value: %c\n", c);
+  printf("C+1 addr: %p\n", c);
+  printf("C+2 addr: %p\n", c);
+
+  /*
+  指標的加減法與一般數值的加減法不同。
+  在指標運算上 +1 -> 表示前進一個資料型態的記憶體長度
+  Ex:在int型態的指標上加一 => 記憶體位址前進4個位元組的長度
+  */
+}
--- Char型別/型別運算.c
+++ Char型別/型別運算.c
@@ -4,8 +4,13 @@
 int main() {
 //宣告字元變數
 //Char 是以ASCII編碼
-  char ch = 'A'+1;
-  printf("%%C=> %c\n", ch);
+  char ch = 'A';
+  printf("%%c=> %c\n", ch);
   printf("%%d=> %d\n", ch);
+
+  for(int i = 0; i < 58; i++) {
+    printf("%c\n", ch);
+    ch += 1;
+  }
   return 0;
 }
