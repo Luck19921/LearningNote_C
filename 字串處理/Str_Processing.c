95a0d437135a35cc52b2ad538accfcc197248d44
Luck19921 <ohsoso27@gmail.com>
2020-12-22 11:35:57 +0800

    字串處理的修正。

--- 字串處理/Str_Processing.c
+++ 字串處理/Str_Processing.c
@@ -16,7 +16,9 @@ int main()
 //puts function
 int main()
 {
+  //宣告char型態 容量為100的字串陣列 並初始化值為Hello world
   char arr[100] = {"Hello world"};
+  //無指定長度資料型態(無正負數之分)len 透過函數strlen去抓上面宣告的字串陣列長度
   unsigned int len = strlen(arr);
   //Sizeof代表著陣列的容量, 而Strlen則是代表陣列內實際包含元素的寬度
   printf("Sizeof Arr:%d\nstrlen(arr):%d\n", sizeof(arr), len);
