8d20de9489d86c7cf0cf7d83296b73ac85e973d6
Luck19921 <ohsoso27@gmail.com>
2020-12-29 19:32:37 +0800

    要加上指標才能呼叫char name"Albert". Why?

--- 定義結構/Classmate(Unfinsh).c
+++ 定義結構/Classmate(Unfinsh).c
@@ -3,7 +3,7 @@
 
 typedef struct myClass myClass;
 struct myClass {
-  //char name;
+  char *name;
   int num; //student number
   int age;
   char group; //Class group ex: A, B, C, D, E
@@ -15,12 +15,12 @@ struct myClass {
 
 int main() {
   myClass instance_A;
-  //instance_A.name = "Albert";
+  instance_A.name = "Albert";
   instance_A.num = 19008421;
-  instnace_A.age = 28;
-  instnace_A.group = 'A';
+  instance_A.age = 28;
+  instance_A.group = 'A';
   instance_A.score_1 = 99;
 
-  printf("Age: %d\n", instance_A.age);
+  printf("Name: %s\n", instance_A.name);
   return 0;
 }
